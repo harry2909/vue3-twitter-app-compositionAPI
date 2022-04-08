e5331a4f258b880ab2f93989f5f9ca5955d44600
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-04-08 14:00:05 +0100

    refactoring for composition + adding router

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="user-profile">
     <div class="user-profile__user-panel">
-      <h1 class="user-profile__username">@{{ user.username }}</h1>
-      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <h1 class="user-profile__username">@{{ state.user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="state.user.isAdmin">
+        Admin
+      </div>
       <div class="user-profile__follower-count">
-        <strong>Followers: </strong> {{ followers }}
+        <strong>Followers: </strong> {{ state.followers }}
       </div>
       <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
         <label for="newTwoot"><strong>New Twoot</strong></label>
-        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
-      <div class="user-profile__create-twoot-type">
-        <label for="newTwootType"><strong>Type: </strong></label>
-        <select id="newTwootType" v-model="selectedTwootType">
-          <option
-            :value="option.value"
-            v-for="(option, index) in twootTypes"
-            :key="index"
-          >
-            {{ option.name }}
-          </option>
-        </select>
-      </div>
-      <button>Twoot!</button>
-       </form>
+        <textarea
+          id="newTwoot"
+          rows="4"
+          v-model="state.newTwootContent"
+        ></textarea>
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="state.selectedTwootType">
+            <option
+              :value="option.value"
+              v-for="(option, index) in state.twootTypes"
+              :key="index"
+            >
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+        <button>Twoot!</button>
+      </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <TwootItem
-        v-for="twoot in user.twoots"
+        v-for="twoot in state.user.twoots"
         :key="twoot.id"
-        :username="user.username"
+        :username="state.user.username"
         :twoot="twoot"
         @favourite="toggleFavourite"
       />
@@ -38,12 +44,13 @@
 
 <script>
 import TwootItem from "./TwootItem.vue";
+import { reactive, computed } from "vue";
 
 export default {
   name: "UserProfile",
   components: { TwootItem },
-  data() {
-    return {
+  setup() {
+    const state = reactive({
       newTwootContent: "",
       selectedTwootType: "instant",
       twootTypes: [
@@ -63,34 +70,38 @@ export default {
           { id: 2, content: "This is a different twood." },
         ],
       },
+    });
+
+    const newTwootCharacterCount = computed(() => state.newTwootContent.length);
+
+    function createNewTwoot() {
+      if (state.newTwootContent && state.selectedTwootType !== "draft") {
+        state.user.twoots.unshift({
+          id: state.user.twoots.length + 1,
+          content: state.newTwootContent,
+        });
+      }
+    }
+
+    return {
+      state,
+      newTwootCharacterCount,
+      createNewTwoot,
     };
   },
   watch: {
     followers(newFollowerCount, oldFollowerCount) {
       if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower!`);
+        console.log(`${this.state.user.username} has gained a follower!`);
       }
-    },
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
     },
   },
   methods: {
     followUser() {
-      this.followers++;
+      this.state.followers++;
     },
     toggleFavourite(id) {
       console.log(`Favourited Tweet ${id}`);
-    },
-    createNewTwoot() {
-      if (this.newTwootContent && this.selectedTwootType !== "draft") {
-        this.user.twoots.unshift({
-          id: this.user.twoots.length + 1,
-          content: this.newTwootContent
-        });
-      }
     },
   },
   mounted() {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './index.css'
+import Home from './views/Home.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Home', component: Home}
+    ]
+});
+
+createApp(App).use(router).mount('#app')
