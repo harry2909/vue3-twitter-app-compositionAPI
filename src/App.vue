ee2563113f99e1fbf2937bd01cbe68686b37aebe
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-04-08 12:31:20 +0100

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile.vue";
+
+export default {
+  name: 'App',
+  components: { UserProfile }
+}
+</script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+      <div class="user-profile__create-twoot-type">
+        <label for="newTwootType"><strong>Type: </strong></label>
+        <select id="newTwootType" v-model="selectedTwootType">
+          <option
+            :value="option.value"
+            v-for="(option, index) in twootTypes"
+            :key="index"
+          >
+            {{ option.name }}
+          </option>
+        </select>
+      </div>
+      <button>Twoot!</button>
+       </form>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favourite="toggleFavourite"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TwootItem from "./TwootItem.vue";
+
+export default {
+  name: "UserProfile",
+  components: { TwootItem },
+  data() {
+    return {
+      newTwootContent: "",
+      selectedTwootType: "instant",
+      twootTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twoot" },
+      ],
+      followers: 0,
+      user: {
+        id: 1,
+        username: "Harry_Preston",
+        firstName: "Harry",
+        lastName: "Preston",
+        email: "harry@gmail.com",
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: "This is a twoot." },
+          { id: 2, content: "This is a different twood." },
+        ],
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+    toggleFavourite(id) {
+      console.log(`Favourited Tweet ${id}`);
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType !== "draft") {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent
+        });
+      }
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
